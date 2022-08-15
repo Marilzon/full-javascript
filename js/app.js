c448241b7e1ddfecf816049694946de8d5cb6d8f
Marilzon <maxmaril@hotmail.com>
2022-08-14 22:16:25 -0300

    maximo denominador comum em javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-"use strict"
+/* Máximo Denominador Comum (MDC), Algoritimo Elclides */
+const mdc = () => {
+    let arr = []
+    let a = prompt("Digite o primeiro numero")
+    let b = prompt("Digite o segundo numero")
+    let result = 0
 
-document.write('<h1>' + 'Executando metodo Write()' + '</h1>')
+    arr.push(a, b)
+    arr.sort()
+
+    do {
+        result = arr[0] % arr[1]
+        arr[0] = arr[1]
+        arr[1] = result
+    } while (result !== 0)
+
+    return arr[0]
+}
 
