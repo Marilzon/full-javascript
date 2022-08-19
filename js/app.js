d25660d5c082b480b1592ff660a8319e25ec963c
Marilzon <maxmaril@hotmail.com>
2022-08-19 10:36:28 -0300

    app.js module to import all functions

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const root = document.querySelector("#root")
+
+import { whyMyInput, sumUpTo } from "./modules/bigO.js"
+import { curring } from "./modules/curring.js"
+import { maximunCommonDenominator } from "./modules/maximunCommonDenominator"
