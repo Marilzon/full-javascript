d9958dd973f1a697d517feb1957e581ed7c7dc4f
Marilzon <maxmaril@hotmail.com>
2022-09-08 15:36:09 -0300

    UDPATE: Removing non javascript folders and files

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const root = document.querySelector("#root")
-
-import { whyMyInput, sumUpTo } from "./modules/bigO.js"
-import { curring } from "./modules/curring.js"
-import { maximunCommonDenominator } from "./modules/maximunCommonDenominator"
